379f1e22eb7fa04d43b5a13206b6b6f6f39fc915
eddyizm <eddyizm@gmail.com>
2017-05-06 13:30:58 -0700

    Added modal pop up for reiki work shop. adding contact form again.

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -7,8 +7,8 @@ if(isset($_POST['message'])){
 	$message = $_POST['message'];
     
 	
-	$to      = 'you@example.com';
-	$subject = 'Site Contact Form';
+	$to      = 'eddyizm@me.com';
+	$subject = 'Reiki Contact Form';
 
 	$headers = 'From: '. $email . "\r\n" .
     'Reply-To: '. $email . "\r\n" .
@@ -23,7 +23,7 @@ if(isset($_POST['message'])){
 		$res['message'] = 'Form Submission Successful';
     }
 	else{
-		$res['message'] = 'Failed to send mail. Please mail me to you@example.com';
+		$res['message'] = 'Failed to send mail. Please mail me to eddyizm@me.com';
 	}
 	
 	
